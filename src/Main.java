475383c3e7dbedc44edee48752a2194e2a660b00
Ksenia <ksenya-yudina@mail.ru>
2024-09-25 10:19:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 52_421;
+        int rubForMiles = 20;
+
+        int freeMile = amount / rubForMiles;
+
+        System.out.println(freeMile);
+    }
+}
